1a82e59bf7b745f3bdf0710768b361118bd4619b
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-17 06:20:50 +0600

    init

--- app/account/models/user.py
+++ app/account/models/user.py
@@ -20,15 +20,16 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, phone, password):
+        """
+        Создаёт суперпользователя.
+        """
+        user = self.create_user(phone, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 ROLE_CLIENT = 'accountant'  # Роль бухгалтера
