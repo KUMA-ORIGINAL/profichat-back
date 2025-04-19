69118e9cda3d944898d37a0bebfe291c98ade23b
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-04-19 22:36:56 +0600

    init

--- app/account/serializers/work_schedule.py
+++ app/account/serializers/work_schedule.py
@@ -3,9 +3,15 @@ from rest_framework import serializers
 from ..models import WorkSchedule
 
 
+class WorkScheduleListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        return WorkSchedule.objects.bulk_create([WorkSchedule(**item) for item in validated_data])
+
+
 class WorkScheduleSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = WorkSchedule
         fields = ('id', 'user', 'day_of_week', 'from_time', 'to_time')
+        list_serializer_class = WorkScheduleListSerializer
--- app/account/views/work_schedule.py
+++ app/account/views/work_schedule.py
@@ -1,5 +1,8 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets, permissions, mixins
+from rest_framework import viewsets, permissions, mixins, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from ..models import WorkSchedule
 from ..serializers import WorkScheduleSerializer
@@ -8,10 +11,39 @@ from ..serializers import WorkScheduleSerializer
 @extend_schema(tags=['Work Schedule'])
 class WorkScheduleViewSet(viewsets.GenericViewSet,
                           mixins.ListModelMixin,
-                          mixins.CreateModelMixin,
                           mixins.UpdateModelMixin):
     serializer_class = WorkScheduleSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return WorkSchedule.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    @action(detail=False, methods=['put', 'patch'], url_path='bulk-update')
+    def bulk_update(self, request):
+        if not isinstance(request.data, list):
+            raise ValidationError("Expected a list of items for bulk update.")
+
+        updated_items = []
+        errors = []
+
+        for item_data in request.data:
+            try:
+                instance = WorkSchedule.objects.get(id=item_data['id'], user=request.user)
+                serializer = self.get_serializer(instance, data=item_data, partial=request.method == 'PATCH')
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                updated_items.append(serializer.data)
+            except Exception as e:
+                errors.append({'id': item_data.get('id'), 'error': str(e)})
+
+        return Response({'updated': updated_items, 'errors': errors})
