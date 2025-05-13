d54067b59276a57fdc99e1e9d4d73c20cc5aef8e
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-05-13 14:52:05 +0600

    init

--- app/chat_access/services/open_banking.py
+++ app/chat_access/services/open_banking.py
@@ -14,7 +14,7 @@ def generate_payment_link(access_order):
         "amount": str(access_order.price),
         "transaction_id": str(access_order.id),
         "comment": f"Оплата заказа #{access_order.id} hospital",
-        "redirect_url": f"https://hospital.operator.kg/",
+        "redirect_url": f"https://profichat.kg/specialist/3",
         'token': PAYMENT_API_TOKEN,
     }
 
