91bf2508a8b93cb29671f3327a31c40d09b3d295
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-06-22 13:28:03 +0600

    fix: register

--- app/account/services/invite_client.py
+++ app/account/services/invite_client.py
@@ -13,8 +13,20 @@ from common.notifications import send_chat_invite_push
 User = get_user_model()
 
 
+def send_invite_sms(client, specialist, chat):
+    invite_link = f"https://profigram.site/r/{chat.channel_id}"
+    text = f"{specialist.first_name} {specialist.last_name[0]} приглашает вас в Profigram — приложение для консультаций. Завершите регистрацию:  {invite_link}"
+    send_sms(phone=client.phone_number, text=text)
+
+
 def invite_client(phone_number: str, tariff_id: int, specialist: User):
     client = User.objects.filter(phone_number=phone_number).first()
+
+    if not client:
+        client = User.objects.create_user(
+            phone_number=phone_number,
+            is_active=False
+        )
 
     chat = Chat.objects.filter(
         client=client,
@@ -42,16 +54,9 @@ def invite_client(phone_number: str, tariff_id: int, specialist: User):
     )
     update_chat_data_from_order(access_order)
 
-    if not client:
-        client = User.objects.create_user(
-            phone_number=phone_number,
-            is_active=False
-        )
-        invite_link = f"https://profigram.site/r/{chat.channel_id}"
-        text = f"{specialist.first_name} {specialist.last_name[0]} приглашает вас в Profigram — приложение для консультаций. Завершите регистрацию:  {invite_link}"
-        send_sms(phone=client.phone_number, text=text)
-
     if client.is_active:
         send_chat_invite_push(client, chat)
+    else:
+        send_invite_sms(client, specialist, chat)
 
     return chat
