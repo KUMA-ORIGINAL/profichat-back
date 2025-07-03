27703de420573b524cc690112b75899b252aa571
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-07-03 18:46:19 +0600

    fix: register

--- app/config/settings.py
+++ app/config/settings.py
@@ -237,7 +237,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
