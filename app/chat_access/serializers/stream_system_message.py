db7c436675f8a53ff3d3555fabb17ce125d17a87
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-07-15 16:53:06 +0600

    fix: register

--- app/chat_access/serializers/stream_system_message.py
+++ app/chat_access/serializers/stream_system_message.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 
 class StreamSystemMessageSerializer(serializers.Serializer):
-    channel_id = serializers.IntegerField(required=True)
+    channel_id = serializers.CharField(required=True)
     custom_type = serializers.CharField(required=True)
     text = serializers.CharField(required=False, allow_blank=True, allow_null=True)
