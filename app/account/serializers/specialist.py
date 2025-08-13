90bf369577e0fc844fa8bba1a793a8985ef9e0a8
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-08-13 17:31:25 +0600

    fix: register

--- app/account/serializers/specialist.py
+++ app/account/serializers/specialist.py
@@ -17,7 +17,7 @@ class SpecialistSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "first_name", "last_name", "phone_number", "photo",
-                  'description', "profession", 'channel_id', 'tariffs', 'work_schedules']
+                  'description', 'can_call', "profession", 'channel_id', 'tariffs', 'work_schedules']
 
     def get_channel_id(self, obj):
         request = self.context.get('request')
